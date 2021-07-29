b672f830c21e99937688c2b3e0e81fef6880c44a
Ganesh <ganesh@noesis-server.local>
2021-07-29 19:18:06 +0530

    blog page added and added sitemap

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,11 +1,17 @@
 module.exports = {
   siteMetadata: {
     siteUrl: "https://www.yourdomain.tld",
-    title: "My Gatsby Site",
+    title: "Ganesh Sawardekar",
   },
   plugins: ["gatsby-plugin-sass", 
   "gatsby-plugin-gatsby-cloud",
   "gatsby-plugin-image",
-  "gatsby-plugin-sharp"
+  "gatsby-plugin-sharp",
+  {
+    resolve: "gatsby-plugin-sitemap",
+    options: {
+      output: `/my-cool-sitemap.xml`,
+    },
+  },
 ],
 };
